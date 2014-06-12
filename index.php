3b38ca9816d9e4932d2d291519e13976db2eeb4e
Alex Dieulot <adieulot@gmail.com>
2014-06-12 20:32:32 +0200

    change folder structure

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $last_ver = '3.0.1';
 $export_folder = '../output';
 
 $pages = array(
-  'welcome',
+  'index',
   'download',
   'click-test',
   'changelog',
@@ -43,16 +43,12 @@ if (defined('STDIN')) {
       include(__FILE__);
     }
 
-    //file_put_contents($export_folder . '/style.css', $style);
-
     copy('script-' . $style_rev . '.js', $export_folder . '/script-' . $style_rev . '.js');
     if (!file_exists($export_folder . '/v' . $last_ver)) {
       mkdir($export_folder . '/v' . $last_ver);
     }
     copy('v' . $last_ver . '/instantclick.js', $export_folder . '/v' . $last_ver . '/instantclick.js');
     copy('v' . $last_ver . '/instantclick.min.js', $export_folder . '/v' . $last_ver . '/instantclick.min.js');
-    copy('twitter.svg', $export_folder . '/twitter.svg');
-    copy('github.png', $export_folder . '/github.png');
     copy('logo.png', $export_folder . '/logo.png');
     copy('favicon.ico', $export_folder . '/favicon.ico');
     copy('scary-analytics.png', $export_folder . '/scary-analytics.png');
@@ -60,29 +56,17 @@ if (defined('STDIN')) {
     copy('change_in_bounce_rate_by_landing_page_speed.jpg', $export_folder . '/change_in_bounce_rate_by_landing_page_speed.jpg');
     copy('302-spam.png', $export_folder . '/302-spam.png');
 
-    $files = array(
-      'index.html',
-      'click-test.html',
-      'download.html',
-      'changelog.html',
-      '3.0.html',
-      'license.html',
-      'github-pages-and-apex-domains.html',
-      'script-' . $style_rev . '.js',
-      'v' . $last_ver . '/instantclick.js',
-      'v' . $last_ver . '/instantclick.min.js',
-      'twitter.svg',
-      'github.png',
-      'logo.png',
-      'favicon.ico',
-      'README.md'
-    );
-
     exit;
   }
 }
 else {
-  $page = isset($_GET['page']) ? $_GET['page'] : 'welcome';
+  $page = isset($_GET['page']) ? substr($_GET['page'], 1) : 'index';
+  if (strlen($page) == 0) {
+    $page = 'index';
+  }
+  if (!in_array($page, $pages)) {
+    $page = '404';
+  }
 }
 
 if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/' . $export_folder . '/') !== false) {
@@ -91,58 +75,56 @@ if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/' . $exp
 }
 
 $titles = array(
-  'welcome' => 'InstantClick — JS library to make your website instant',
+  'index' => 'InstantClick — JS library to make your website instant',
   'download' => 'Download InstantClick',
   'click-test' => 'Test your click speed - InstantClick',
   'changelog' => 'Changelog - InstantClick',
   '3.0' => 'InstantClick 3.0 Released',
   'license' => 'MIT License - InstantClick',
-  'github-pages-and-apex-domains' => 'GitHub Pages is slow with root domains - InstantClick'
+  'github-pages-and-apex-domains' => 'GitHub Pages is slow with root domains - InstantClick',
+  '404' => 'Page not found',
 );
 
 $descriptions = array(
-  'welcome' => 'InstantClick makes following links in your website instant.',
+  'index' => 'InstantClick makes following links in your website instant.',
   'download' => 'Download and get started with InstantClick.',
   'click-test' => 'Tells you the delay between your hover/mousedown and click.',
   'changelog' => 'InstantClick’s progress across versions, release notes.',
   '3.0' => 'Release announcement for InstantClick 3.0: Preloading for mobile devices, progress bar.',
   'license' => 'InstantClick is released under the MIT License.',
-  'github-pages-and-apex-domains' => 'GitHub Pages with a custom root domain is terribly slow. It will make 35% of your visitors go away.'
+  'github-pages-and-apex-domains' => 'GitHub Pages with a custom root domain is terribly slow. It will make 35% of your visitors go away.',
 );
+
+if ($page == '404') {
+  header('HTTP/1.1 404 Not Found');
+}
 ?>
 <!doctype html>
 <meta charset="utf-8">
 <title><?php echo $titles[$page] ?></title>
-<meta name="viewport" content="width=750">
+<meta name="viewport" content="width=768">
 <style><?php echo $style ?></style>
 <meta name="description" content="<?php echo $descriptions[$page] ?>">
 <?php if ($page != '3.0'): ?>
-<link rel="canonical" href="http://instantclick.io/<?php if ($page != 'welcome') { echo $page; } ?>">
+<link rel="canonical" href="http://instantclick.io/<?php if ($page != 'index') { echo $page; } ?>">
 <?php endif ?>
 
-<div id="header">
+<header id="header">
   <h1><a href=".">InstantClick</a></h1>
   <ul>
-    <li class="github"><a href="https://github.com/dieulot/instantclick" title="GitHub">Github</a>
-    <li class="twitter"><a href="https://twitter.com/instantclickjs" title="Twitter">Twitter</a>
-<?php
-$menu_items = array(
-  array('welcome', 'Home'),
-  array('download', 'Download'),
-  array('click-test', 'Click test'),
-  array('changelog', 'Changelog')
-);
-foreach ($menu_items as $item): ?>
-    <li<?php if ($item[0] == $page || ($page == '3.0' && $item[0] == 'changelog')) { echo ' class="on"'; } ?>><a href="<?php echo $item[0] == 'welcome' ? '.' : $item[0]; ?>"><?php echo $item[1] ?></a>
-<?php endforeach ?>
+    <li><a href="/download">Download</a>
+    <li><a href="/click-test">Click test</a>
+    <li><a href="/blog">Blog</a>
   </ul>
-</div>
-<?php include($page . '.html') ?>
+  <div class="border"></div>
+</header>
+<article class="container">
+<?php include('pages/' . $page . '.html') ?>
+</article>
 <div id="footer">InstantClick is released under the <a href="license">MIT License</a>, © 2014 Alexandre Dieulot</div>
 <script src="script-<?php echo $style_rev ?>.js" data-no-instant></script>
 <?php
 if (defined('STDIN')) {
-  if ($page == 'welcome') $page = 'index';
   $contents =  ob_get_contents();
   ob_end_clean();
   file_put_contents($export_folder . '/' . $page . '.html', $contents);
